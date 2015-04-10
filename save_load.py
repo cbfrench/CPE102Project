7bd345a317d3d73b1c5b209e632251b33130bc39
Connor <connorfrench716@gmail.com>
2015-04-10 12:34:46 -0700

    Finished worldmodel file

--- save_load.py
+++ save_load.py
@@ -57,7 +57,7 @@ def save_world(world, file):
    save_background(world, file)
 
 def save_entities(world, file):
-   for entity in worldmodel.get_entities(world):
+   for entity in world.get_entities():
       if isinstance(entity, MinerFull) or isinstance(MinerNotFull):
          file.write(entity.miner_string() + '\n')
       elif isinstance(entity, Blacksmith):
@@ -76,7 +76,7 @@ def save_background(world, file):
    for row in range(0, world.num_rows):
       for col in range(0, world.num_cols):
          file.write('background ' +
-            (worldmodel.get_background(world, point.Point(col, row))) +
+            (world.get_background(point.Point(col, row))) +
             ' ' + str(col) + ' ' + str(row) + '\n').get_name()
 
 def load_world(world, images, file, run=False):
@@ -93,14 +93,14 @@ def add_background(world, properties, i_store):
    if len(properties) >= BGND_NUM_PROPERTIES:
       pt = point.Point(int(properties[BGND_COL]), int(properties[BGND_ROW]))
       name = properties[BGND_NAME]
-      worldmodel.set_background(world, pt,
+      world.set_background(pt,
          entities.Background(name, image_store.get_images(i_store, name)))
 
 
 def add_entity(world, properties, i_store, run):
    new_entity = create_from_properties(properties, i_store)
    if new_entity:
-      worldmodel.add_entity(world, new_entity)
+      world.add_entity(new_entity)
       if run:
          schedule_entity(world, new_entity, i_store)
 
